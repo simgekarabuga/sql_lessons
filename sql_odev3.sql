2a1f8f4a9d1e83851d23d4c9ea127bd8e3824dc4
simgekarabuga <109412933+simgekarabuga@users.noreply.github.com>
2023-09-29 20:09:15 +0300

    sql lessons
    sql homework 3

---
+++ sql_odev3.sql
@@ -0,0 +1,8 @@
+--select * from country
+--where country like 'A%a'
+--select * from country
+--where country like '______n'
+--select title from film
+--where title ilike '%t%t%t%t'
+select * from film
+where title like 'C%' and length >90 and rental_rate = 2.99
