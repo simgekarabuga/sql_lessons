2d35060b1ac1df303d8533eb8acb06c4e0e5d8fe
simgekarabuga <109412933+simgekarabuga@users.noreply.github.com>
2023-09-29 23:26:16 +0300

    sql homework 4
    sql lessons homework 4

---
+++ sql_odev4.sql
@@ -0,0 +1,8 @@
+--select distinct replacement_cost from film
+--select count(distinct replacement_cost) from film 
+--select * from film
+--where title like 'T%' and rating ='G'
+--select country from country
+--where country like '_____'
+select city from city
+where city ilike '%r'
